ae7a07447f673911068877f15215c1403a9aa1ab
user <Klimovich.maks@gmail.com>
2022-09-15 13:00:25 +0300

    add Final test work

--- TestWork/Program.cs
+++ TestWork/Program.cs
@@ -1,2 +1,32 @@
 ﻿
-Console.WriteLine("Hello, World!");
+string[] daysOfWeek = {"Monday", "Tuersday", "Wednesday", "Thirsday", "Friday", "Saturday", "Sunday" };
+
+void PrintDays(string[] array)
+{
+    Console.Write("[");
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("] -> ");
+}
+
+void PrintThreeDays(string[] arr)
+{
+    Console.Write("[");
+    int count = new Random().Next(0, 3);
+    for (int i = 0; i <= count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.Write("]");
+}
+
+
+PrintDays(daysOfWeek);
+PrintThreeDays(daysOfWeek);
+
+
+
+
